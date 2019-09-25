5c6e8fcf38356d78a2c951c0f54321ca94f69385
sinnim7 <sinnim7@gmail.com>
2019-09-25 18:53:06 +0900

    0925

---
+++ bitcamp-java-spring-webmvc/src/main/java/bitcamp/car/Car.java
@@ -0,0 +1,5 @@
+package bitcamp.car;
+
+public interface Car {
+
+}
---
+++ bitcamp-java-spring-webmvc/src/main/java/bitcamp/car/Sonata.java
@@ -0,0 +1,10 @@
+package bitcamp.car;
+
+public class Sonata implements Car{
+
+  
+  public void run() {
+    System.out.println("sonata.run()....호출됨!");
+  }
+  
+}
